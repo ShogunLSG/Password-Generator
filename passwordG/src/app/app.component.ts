b56f0da1c061e225b3ea35c0329e02eea8e23baf
Lesego Matsimela <lesegomat@harambee.co.za>
2023-04-19 15:07:50 +0200

    event handler logic for input

--- passwordG/src/app/app.component.ts
+++ passwordG/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordG';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  passwordLength = 0;
+
+
+  onButtonClick() {
+    this.password = 'password1';
+    console.log(`
+      About to generate a password with the following:
+      Include letters: ${this.includeLetters}
+      Include numbers: ${this.includeNumbers}
+      Include symbols: ${this.includeSymbols}
+      `)
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onLengthInput(event: Event){
+    const passwordLength = parseInt((event.target as HTMLInputElement).value);
+
+  }
 }
